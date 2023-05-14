52589df4e7784e4f95c33e173f89c9444f78e4ba
Tobias <tgoettel9401@gmail.com>
2023-05-14 12:46:41 +0200

    Change generation URL to generate instead of generateNew

--- src/main/java/org/dhbw/webapplicationgenerator/webclient/ProjectController.java
+++ src/main/java/org/dhbw/webapplicationgenerator/webclient/ProjectController.java
@@ -3,8 +3,6 @@ package org.dhbw.webapplicationgenerator.webclient;
 import lombok.AllArgsConstructor;
 import org.dhbw.webapplicationgenerator.model.request.ProjectRequest;
 import org.dhbw.webapplicationgenerator.webclient.exception.ValidationException;
-import org.dhbw.webapplicationgenerator.webclient.exception.WagException;
-import org.dhbw.webapplicationgenerator.webclient.request.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,8 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.util.HashSet;
-import java.util.Set;
 
 @RestController
 @AllArgsConstructor
@@ -26,8 +22,8 @@ public class ProjectController {
     private final ProjectRequestTransformer projectRequestTransformer;
     private final ProjectRequestValidator projectRequestValidator;
 
-    @PostMapping("generateNew")
-    public byte[] generateByRequestNew(@RequestBody() ProjectRequest request) throws ValidationException, IOException {
+    @PostMapping("generate")
+    public byte[] generateByRequest(@RequestBody() ProjectRequest request) throws ValidationException, IOException {
         logger.info("Received generation request for project with title {}", request.getTitle());
         logger.info("Validating the request");
         projectRequestTransformer.transform(request);
